a3aa7959f96651288f04829fe58ce0871e9ad79d
md-y <15069105+md-y@users.noreply.github.com>
2025-05-06 19:36:12 -0500

    Fix door

--- Assets/CustomScript/door_control.cs
+++ Assets/CustomScript/door_control.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(NetworkObject))]
+[RequireComponent(typeof(NetworkTransform))]
 public class DoorControl : NetworkBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     Vector3 close_position = new Vector3(2f, 0.0f, -1f);
@@ -12,24 +13,19 @@ public class DoorControl : NetworkBehaviour, IPointerEnterHandler, IPointerExitH
 
     bool door_open = false;
 
-    void OpenDoor()
+    NetworkTransform networkTransform;
+
+    void Start()
     {
-        transform.position = open_position;
-        transform.rotation = Quaternion.Euler(open_rotation);
-        door_open = true;
-    }
-    void CloseDoor()
-    {
-        transform.position = close_position;
-        transform.rotation = Quaternion.Euler(close_rotation);
-        door_open = false;
+        networkTransform = GetComponent<NetworkTransform>();
     }
 
     [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
     void RpcToggleDoor()
     {
-        if (door_open) CloseDoor();
-        else OpenDoor();
+        if (door_open) networkTransform.Teleport(close_position, Quaternion.Euler(close_rotation));
+        else networkTransform.Teleport(open_position, Quaternion.Euler(open_rotation));
+        door_open = !door_open;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -43,7 +39,7 @@ public class DoorControl : NetworkBehaviour, IPointerEnterHandler, IPointerExitH
     public void OnPointerClick(PointerEventData eventData)
     {
         var offset = eventData.enterEventCamera.transform.position - transform.position;
-        if (offset.magnitude < 4)
+        if (offset.magnitude < 8)
         {
             RpcToggleDoor();
         }
