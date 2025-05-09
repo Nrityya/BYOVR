db3f7c2322544812de18a7e1c66fb42f143d021f
derek <dezman18@Outlook.com>
2025-05-09 04:12:20 -0500

    final animation changes + cleanup of old scripts

--- Assets/CustomScript/PlayerInputAnimator.cs
+++ Assets/CustomScript/PlayerInputAnimator.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Fusion;
+using UnityEngine.EventSystems;
 
 public class PlayerInputAnimator : NetworkBehaviour
 {
@@ -11,19 +12,27 @@ public class PlayerInputAnimator : NetworkBehaviour
     }
 
     void Update()
-    {
+    {   
         if (!HasInputAuthority) return;
         // A in controlelr
-        if (ControllerInputHelper.IsADown())
+        if (ControllerInputHelper.IsADown()|| Input.GetKeyDown(KeyCode.P))
         {
-            Debug.Log("P pressed");
+            // Debug.Log("P pressed");
             networkAnimator.SetTrigger("Point", true);
         }
         // Y in controller
-        if (ControllerInputHelper.IsYButtonDown())
+        if (ControllerInputHelper.IsYButtonDown() || Input.GetKeyDown(KeyCode.N))
         {
-            Debug.Log("N pressed");
+            // Debug.Log("N pressed");
             networkAnimator.SetTrigger("Wave", true);
+        } 
+        if(Mathf.Abs(Input.GetAxis("Horizontal"))>0.1f || Mathf.Abs(Input.GetAxis("Vertical"))>0.1f)
+        {   
+            // Debug.Log("Moving");
+            networkAnimator.SetTrigger("Walk", true);
+        }else{
+            // Debug.Log("Not Moving");
+            networkAnimator.SetTrigger("WalkOff", true);
         }
     }
 }
